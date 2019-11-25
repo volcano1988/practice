495733627e2822adf51284aeebf31d627ec5c3b0
cuilongcan <cui.longcan@chinaott.net>
2019-11-25 11:39:00 +0800

    leetcode daily

---
+++ practice-leetcode/src/main/java/com/cc/leetcode/DeleteNodeInLinkedList.java
@@ -0,0 +1,18 @@
+package com.cc.leetcode;
+
+import com.cc.common.base.ListNode;
+
+/**
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+ *
+ * @author cuilongcan <cui.longcan@chinaott.net>
+ * @create 2019-11-25
+ */
+public class DeleteNodeInLinkedList {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
