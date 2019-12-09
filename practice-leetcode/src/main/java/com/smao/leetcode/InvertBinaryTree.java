a254f5d9c53b1cac4982d11d971f3980484e13fe
jinlulu <jinlulu@95303.com>
2019-12-09 10:38:43 +0800

    翻转二叉树

---
+++ practice-leetcode/src/main/java/com/smao/leetcode/InvertBinaryTree.java
@@ -0,0 +1,54 @@
+package com.smao.leetcode;
+
+import com.cc.common.base.TreeNode;
+
+/**
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ *
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ *
+ * 链接：https://leetcode-cn.com/problems/invert-binary-tree
+ */
+public class InvertBinaryTree {
+    public static TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
+        return root;
+    }
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(4);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(7);
+        treeNode.left.left = new TreeNode(1);
+        treeNode.left.right = new TreeNode(3);
+        treeNode.right.left = new TreeNode(6);
+        treeNode.right.right = new TreeNode(9);
+        TreeNode tr = invertTree(treeNode);
+        System.out.println("11111111");
+    }
+}
