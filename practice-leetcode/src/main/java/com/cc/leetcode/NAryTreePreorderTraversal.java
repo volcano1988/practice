3cab30dc6eba2a4c0d7478f8eadd5099f3172f57
cuilongcan <cui.longcan@chinaott.net>
2019-12-19 09:54:40 +0800

    practice spring5 自定义模块

---
+++ practice-leetcode/src/main/java/com/cc/leetcode/NAryTreePreorderTraversal.java
@@ -0,0 +1,30 @@
+package com.cc.leetcode;
+
+import com.cc.common.base.Node;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个 N 叉树，返回其节点值的前序遍历。
+ *
+ * @author cuilongcan <cui.longcan@chinaott.net>
+ * @create 2019-12-19
+ */
+public class NAryTreePreorderTraversal {
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new LinkedList<>();
+        if (root == null) {
+            return list;
+        }
+
+            list.add(root.val);
+        if (root.children!=null) {
+            for (Node child : root.children) {
+                list.addAll(preorder(child));
+            }
+        }
+        return list;
+    }
+}
---
+++ practice-spring5/practice-spring5-initializer/src/main/java/com/practice/spring5/initializer/CustomInitializer.java
@@ -0,0 +1,17 @@
+package com.practice.spring5.initializer;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * 自定义bean加载器
+ *
+ * @author cuilongcan <cui.longcan@chinaott.net>
+ * @create 2019-12-18
+ */
+public class CustomInitializer implements ApplicationContextInitializer {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+
+    }
+}
